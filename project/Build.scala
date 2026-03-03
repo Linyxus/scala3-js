aa67c644a7942ce74952687610824ae3f6cdf615
Yichen Xu <yichen.x@outlook.com>
2026-03-04 01:30:04 +0800

    Milestone: javascript linking works

--- project/Build.scala
+++ project/Build.scala
@@ -1837,6 +1837,7 @@ object Build {
         baseDirectory.value / "src",                                // overrides first
         (LocalProject("scala3-compiler-bootstrapped") / baseDirectory).value / "src",               // shared compiler sources
         (LocalProject("scala3-compiler-bootstrapped") / baseDirectory).value / "src-bootstrapped",  // bootstrapped-only sources
+        (LocalProject("tasty-core-bootstrapped") / baseDirectory).value / "src",                    // tasty-core sources (need sjsir)
       ),
       // Exclude JVM-only packages and Java files, and apply override mechanism
       Compile / sources := {
@@ -1919,7 +1920,9 @@ object Build {
       target := target.value / "scala3-compiler-sjs",
       publish / skip := true,
       bspEnabled := false,
-      Compile / mainClass := None,
+      scalaJSUseMainModuleInitializer := true,
+      Compile / mainClass := Some("dotty.tools.dotc.Main"),
+      scalaJSLinkerConfig ~= { _.withESFeatures(_.withESVersion(ESVersion.ES2018)) },
     )
 
   // ==============================================================================================
